c930f42405354d4734a70d88f389a5f8e5506ed3
chinen <chinen@tam-tam.co.jp>
2020-10-08 20:34:35 +0900

    add: first commit

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <main className="main">
+      <p className="text">ページがありません！</p>
+      <p className="text">ページがありません！</p>
+    </main>
+  );
+}
